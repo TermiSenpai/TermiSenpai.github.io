72360d309efdb0278a118a3f123f60980b0780a7
TermiSenpai <escarpaprieto@gmail.com>
2024-06-19 10:14:36 +0200

    Better javascript

---
+++ js/data.js
@@ -0,0 +1,154 @@
+const educationEntries = [
+    {
+        en: {
+            title: "Software development",
+            institution: "Krono Ofimática S.L.",
+            date: "2021 - 2022"
+        },
+        es: {
+            title: "Programación de sistemas informáticos",
+            institution: "Krono Ofimática S.L.",
+            date: "2021 - 2022"
+        }
+    },
+    {
+        en: {
+            title: "Master's in Game Development and virtual reality with Unity",
+            institution: "Tokio School",
+            date: "2022 - 2024"
+        },
+        es: {
+            title: "Master en programación de videojuegos y realidad virtual con Unity",
+            institution: "Tokio School",
+            date: "2022 - 2024"
+        }
+    },
+    {
+        en: {
+            title: "Master's in Game Development and Virtual Reality with Unreal Engine",
+            institution: "Tokio School",
+            date: "2023 - Present"
+        },
+        es: {
+            title: "Master en desarrollo de videojuegos y realidad virtual con Unreal Engine",
+            institution: "Tokio School",
+            date: "2023 - Actualmente"
+        }
+    }
+];
+
+
+const experienceEntries = [
+    {
+        title: {
+            en: "Software Developer at Austral-Robotics",
+            es: "Desarrollador de software en Austral-Robotics"
+        },
+        description: {
+            en: "Design and development of a robotics simulator in Unity, focusing on remote software development tasks and problem-solving.",
+            es: "Planteamiento, diseño y desarrollo de simulador de robótica en Unity, centrado en tareas de desarrollo de software remoto y resolución de problemas."
+        },
+        date: {
+            en: "January 2024 - March 2024",
+            es: "Enero 2024 - Marzo 2024"
+        }
+    },
+    {
+        title: {
+            en: "Software developer at TALLERES PALACIO AROCA S.L.",
+            es: "Desarrollador de software en TALLERES PALACIO AROCA S.L."
+        },
+        description: {
+            en: "Designed and developed a desktop application with CRUD operations connected to a database.",
+            es: "Planteamiento y desarrollo de una aplicación de escritorio con un sistema CRUD relacionada con una base de datos."
+        },
+        date: {
+            en: "October 2021 - November 2021",
+            es: "Octubre 2021 - Noviembre 2021"
+        }
+    }
+];
+
+
+const codingSkills = [
+    {
+        en: "C# Programming",
+        es: "Programación en C#"
+    },
+    {
+        en: "Unity Development",
+        es: "Desarrollo con Unity"
+    },
+    {
+        en: "Software Design",
+        es: "Diseño de software"
+    },
+    {
+        en: "Software Documentation",
+        es: "Documentación de programas informáticos"
+    },
+    {
+        en: "Technical Documentation",
+        es: "Documentación técnica"
+    },
+    {
+        en: "Multithreading",
+        es: "Desarrollo multihilo"
+    },
+    {
+        en: "Optimization",
+        es: "Optimización"
+    },
+    {
+        en: "Object-Oriented Programming (OOP)",
+        es: "Programación Orientada a Objetos (POO)"
+    },
+    {
+        en: "SOLID Principles",
+        es: "Principios SOLID"
+    },
+    {
+        en: "Game Design",
+        es: "Diseño de videojuegos"
+    },
+    {
+        en: "C++ Programming",
+        es: "Programación en C++"
+    },
+    {
+        en: "JavaScript",
+        es: "JavaScript"
+    },
+    {
+        en: "Python Programming",
+        es: "Programación en Python"
+    },
+    {
+        en: "GIT Version Control",
+        es: "Control de versiones con GIT"
+    }
+];
+
+
+const professionalSkills = [
+    {
+        en: "Project Management",
+        es: "Gestión de Proyectos"
+    },
+    {
+        en: "Team Collaboration",
+        es: "Colaboración en Equipo"
+    },
+    {
+        en: "Problem Solving",
+        es: "Resolución de Problemas"
+    },
+    {
+        en: "Game Design",
+        es: "Diseño de Videojuegos"
+    },
+    {
+        en: "Self-Taught Learning",
+        es: "Aprendizaje Autodidacta"
+    }
+];
