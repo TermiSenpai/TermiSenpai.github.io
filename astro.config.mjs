f07bac6149e92002cc8ae475bd54746f49e2eea0
TermiSenpai <escarpaprieto@gmail.com>
2024-11-09 13:04:27 +0100

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://TermiSenpai.github.io/TermiSenpai.github.io/',
+  base: '/TermiSenpai.github.io/'  // Asegura que todos los enlaces y rutas funcionen correctamente
+});
